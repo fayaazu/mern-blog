c2129383f683ab87fd77bb640dfff4db69ecc24a
fayaazu <fayaazu.290@gmail.com>
2025-01-16 19:20:54 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import Dashboard from './Pages/Dashboard';
+import About from './Pages/About'
+import Projects from './Pages/Projects';
+import SignUp from './Pages/SignUp';
+import SignIn from './Pages/SignIn';
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
+    <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
+
+  // return(
+  //   <div>
+  //     Hello world
+  //   </div>
+  // );
 }
---
+++ client/src/Pages/Projects.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+// export default function Projects() {
+//   return (
+//     <div>Projects</div>
+//   )
+// }
+
+
+import React from 'react';
+
+// export default function Projects() {
+//   return (
+//     <div>
+//       <h1>Hello world HIonoin oopinsfi oihoifha</h1>
+//     </div>
+//   );
+// }
+
+export default function Projects() {
+  return <h1>Projects Page Test</h1>;
+}
